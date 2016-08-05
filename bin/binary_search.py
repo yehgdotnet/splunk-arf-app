15259bb9ae7c258052a907046977694b393adfc3
Jason Garman <jgarman@bit9.com>
2016-08-05 10:50:51 -0400

    Integrate Kyle's changes, reformatting, and add 3rd party LICENSE

--- bin/binary_search.py
+++ bin/binary_search.py
@@ -8,6 +8,8 @@ import os
 import sys
 from splunklib.searchcommands import dispatch, GeneratingCommand, Configuration, Option
 import splunk.clilib.cli_common
+import time
+
 
 @Configuration()
 class BinarySearchCommand(GeneratingCommand):
@@ -22,15 +24,24 @@ class BinarySearchCommand(GeneratingCommand):
     def prepare(self):
         configuration_dict = splunk.clilib.cli_common.getConfStanza('carbonblack', 'cbserver')
 
-        cb_server = configuration_dict['cburl']
+        self.cb_server = configuration_dict['cburl']
         token = configuration_dict['cbapikey']
 
-        self.cb = CbApi(cb_server, token=token, ssl_verify=False)
+        self.cb = CbApi(self.cb_server, token=token, ssl_verify=False)
 
     def generate(self):
         for bindata in self.cb.binary_search_iter(self.query):
             self.logger.info("yielding binary %s" % bindata["md5"])
-            yield dict((field_name, bindata.get(field_name, "")) for field_name in self.field_names)
+            rawdata = dict((field_name, bindata.get(field_name, "")) for field_name in self.field_names)
+        try:
+            rawdata
+            synthevent = {'sourcetype': 'bit9:carbonblack:json', '_time': time.time(), 'source': self.cb_server,
+                          '_raw': rawdata}
+            yield synthevent
+        except Exception:
+            synthevent = {'sourcetype': 'bit9:carbonblack:json', '_time': time.time(), 'source': self.cb_server,
+                          '_raw': '{"Error":"MD5 not found"}'}
+            yield synthevent
 
 
 if __name__ == '__main__':
